82c0dc491b1c7dd1304e622466a3ab10bb865642
Kirill <KRLY05@gmail.com>
2017-09-21 18:40:31 +0200

    Simplified version

--- Curve_fitting_TabPy.py
+++ Curve_fitting_TabPy.py
@@ -1,20 +1,17 @@
 SCRIPT_REAL('
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import scipy.interpolate as interpolate
 
-X=_arg1 #assigning months dimension to x axis
-y=_arg2 #assigning our KPI measure to y axis
+X_all=_arg1 #assigning date dimension to x axis
+y_all=_arg2 #assigning our KPI measure to y axis
 
-#getting rid from Nones
-X=[i for i in X if i is not None]
-y=[i for i in y if i is not None]
+y=[i for i in y_all if i is not None] #getting rid from Nones for y
+X=[i[0] for i in enumerate(X_all) if i[1] is not None] #getting list of indexes for X without Nones
 
-xsmooth=list(range(min(X),max(X)+1)) #densification of X axis (can be done as well with numpy.linespace)
-ysmooth=interpolate.pchip_interpolate(X,y,xsmooth) #interpolation (depending on source data, other function can be used, like spline)
+Xsmooth=list(range(0,len(X_all))) #generation full list of indexes for X
+ysmooth=interpolate.pchip_interpolate(X,y,Xsmooth) #interpolation (depending on source data, other function can be used, like spline)
 
 result=[round(i,10) for i in ysmooth] #rounding to fit in output JSON
 return result
 ',
-attr([Month]),[ASK vFP])
+
+attr([Acg Date]),[ASK vFP])
